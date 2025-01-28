d00b19a8a3cd0f4b93f21edfdbc96d1f4442d109
adityabchickmath <aditya.chickmath@gmail.com>
2025-01-27 21:16:47 -0500

    Added Movies and Description Page

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.movie_title
+
+class Description(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    description = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.description
+
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+def home(request):
+    latest_movie_list = Movie.objects.all()
+    context = {
+        "latest_movie_list": latest_movie_list,
+    }
+    # output = ", ".join([m.movie_title for m in latest_movie_list])
+
+    return render(request, "movies/index.html", context)
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, "movies/detail.html", {"movie": movie})
