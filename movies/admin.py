342616b19046291af0d5fbd098a20c62be9459bc
adityabchickmath <aditya.chickmath@gmail.com>
2025-02-16 16:19:28 -0500

    sthylish

--- movies/admin.py
+++ movies/admin.py
@@ -5,5 +5,9 @@ from .models import Description
 admin.site.register(Movie)
 admin.site.register(Description)
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
