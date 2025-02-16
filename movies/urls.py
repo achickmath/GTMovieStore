c9e569550375da094d95963592ac096970bacf19
adityabchickmath <aditya.chickmath@gmail.com>
2025-02-16 16:09:22 -0500

    stylish

--- movies/urls.py
+++ movies/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path("home/<int:movie_id>/", views.detail, name="detail"),
     path("loggingout/", views.loggingout,name="loggingout"),
     path("home/cart/", views.cart, name="cart"),
+    path('', views.index, name='movies.index'),
+    path('<int:id>/', views.show, name='movies.show'),
+    path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
+    path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
+    path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='movies.delete_review'),
 ]
